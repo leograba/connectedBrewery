bf2e37bb2634bfc45a55053a6b634f1793f1e3cb
Leonardo Graboski Veiga <leogveiga@gmail.com>
2019-07-11 22:51:44 -0300

    migrate: migrate all files from final_paper_tcc
    This project is the follow-up of final_paper_tcc:
    https://github.com/leograba/final_paper_tcc
    
    First thing to be done from now on is a cleanup

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# connectedBrewery
-This is the project of a connected brewery
+# final_paper_tcc
+This repo contains some codes used in my final work for the undergraduation
+
+The *Develop* branch is the default branch for development purposes, so it holds the most up-to-date files, but there is
+no certainty whether there is something not working or unfinished.
+
+## About the node modules installed
+
+    /var/www/node_modules
+
+
+**Module**           | **Status/Info**
+---------------------|-------------
+body-parser          | middleware used within Express
+bonescript           | deprecated &rarr; use octalbonescript
+debug                | no need since Express has it
+ds18b20              | currently not in use
+express              | in use
+express-generator    | not sure
+node-pru-extended    | in use
+octalbonescript      | in use
+socket.io            | currently not in use (big potential)
+update               | not sure
+
+    /var/www/controle/node_modules
+
+**Module**           | **Status/Info**
+---------------------|-------------
+body-parser          | middleware used within Express
+cookie-parser        | not sure
+debug                | not sure
+express              | not sure
+jade                 | not sure
+morgan               | not sure
+serve-favicon        | not sure / maybe
+
+The web application relevant file structure is as follows (notice there may be more relevant files, since
+I didn`t tested these are the only relevant ones):
+
+	|-buttons.css
+	|-config.css
+	|-controle.html
+	|-controle.js
+	|-develop.html
+	|-dyn_graph2.html
+	|-figuras/
+	| |
+	| |----beer2.png
+	| |----beer_compiler.png
+	|-form.css
+	|-header.php
+	|-index2.php
+	|-index2.html
+	|-index.html
+	|-restricted/
+	| |
+	| |----deleterecipe.php
+	| |----listareceita.php
+	| |----listrecipe.js
+	| |----menu_receitas.html
+	| |----newrecipe.js
+	| |----newrecipe.php
+	| |----novareceita.php
+	| |----previewrecipe.php
+	| |----recipes/
+	| |----teste.php
+	|-startrecipe.html
+	
---
+++ www/about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="./css/config.css">
+    <link rel="stylesheet" type="text/css" href="./css/buttons.css">
+    <link rel="icon" type="image/png" href="./img/beer2.png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="./lib/header.js"></script>
+	<script> 
+		$(function(){headerPHP("./lib/header.php");});//add header 
+	</script>
+</head>
+
+<body style="display:none;">
+    <?php // include './lib/header.php'; echo generateHeader();?>
+    <h1>Cervejaria do Futuro</h1>
+    <p>Trabalho de Conclusão de Curso de Engenharia Elétrica - Ênfase em Eletrônica</p>
+    <p>Escola de Engenharia de São Carlos - Universidade de São Paulo (EESC-USP)</p>
+    <p>Professor orientador: Evandro Luís Linhari Rodrigues</p>
+    <p>Aluno: Leonardo Graboski Veiga</p>
+    <ul>
+        <li>Email: leogveiga@gmail.com</li>
+        <li>Github: <a style="font-size:1em;" href="https://github.com/leograba/final_paper_tcc">https://github.com/leograba/final_paper_tcc</a></li>
+    </ul>
+    
+    <br>
+
+    <p>Objetivo do Projeto: Desenvolver um sistema inovador de produção de cerveja
+    , fácil de usar, divertido, confiável e disponível em qualquer lugar</p>
+    <br>
+    
+    <p style="font-size:0.75em;">Esta interface foi desenvolvida para ser visualizada em desktop (1366x768 - resolução WXGA), com o navegador Google Chrome.
+    Outros navegadores, resoluções e/ou dispositivos móveis <i>podem</i> apresentar problemas de design</p>
+    
+    <h2>Testes preliminares</h2>
+    <iframe width="420" height="315" src="https://www.youtube.com/embed/9UafTnT6YmY" frameborder="0" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/LiZ8gi9YoFU" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
